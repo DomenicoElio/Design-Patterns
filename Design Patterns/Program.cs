a8e379466b656eaebd9b424f52a5a6bd8a11c313
domenico.bressanello <domenico.bressanello@avanade.com>
2022-09-07 16:34:16 +0200

    sample singleton program implementing thread locking (as a normal singleton pattern may not be thread safe) - example program rewritten and explained

--- Design Patterns/Program.cs
+++ Design Patterns/Program.cs
@@ -1,8 +1,38 @@
-﻿using System;
+﻿using Design_Patterns.Singleton_Pattern;
+using System;
 internal class Program
 {
+
+    public static void TestSingleton(string value)
+    {
+        Singleton singleton = Singleton.GetInstance(value);
+        Console.WriteLine(singleton.Value);
+    }
     private static void Main(string[] args)
     {
-        ;
+        //     ------------------------------Singleton------------------------------
+
+        Console.WriteLine(
+            "{0}\n{1}\n\n{2}\n",
+            "same values means singleton was reused (check comments about lock function to understand) (yay!)",
+            "If you see different values, then 2 singletons were created (booo!!) (thread locking didnt work properly)",
+            "RESULT:"
+        );
+
+        Thread firstProcess = new Thread(() => { 
+            TestSingleton("FOO");
+        }); // first process launched
+        Thread secondProcess = new Thread(() => {
+            TestSingleton("BAR");
+        }); // second process launched
+
+        // whichever of the two arrived at the thread and triggers the lock first, will print twice and leave 
+        // the other process stuck at the lock. Output will therefore be: BAR BAR or FOO FOO . 
+
+        firstProcess.Start();
+        secondProcess.Start();
+
+        firstProcess.Join();
+        secondProcess.Join();
     }
 }
---
+++ Design Patterns/Singleton Pattern/Singleton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Singleton_Pattern
+{
+    class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _instance;
+
+        // lock obj necessary to synchronize threads on first singleton access
+     
+        private static readonly object _lock = new object();
+
+        public static Singleton GetInstance(string value)
+        {
+            // This conditional is needed to prevent threads stumbling over the
+            // lock once the instance is ready.
+            if (_instance == null)
+            {
+                // Since there's no Singleton instance yet, multiple threads can
+                // simultaneously pass the previous conditional and reach this
+                // point almost at the same time. The first of them will acquire
+                // lock and will proceed further, while the rest will wait.
+                lock (_lock)
+                {
+                    // The first thread to acquire the lock, reaches this
+                    // conditional, goes inside and creates the Singleton
+                    // instance. Once it leaves the lock block, a thread that
+                    // might have been waiting for the lock release may then
+                    // enter this section. But since the Singleton field is
+                    // already initialized, the thread won't create a new object.
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                        _instance.Value = value;
+                    }
+                }
+            }
+            return _instance;
+        }
+        public string Value { get; set; }
+    }
+
+}
+
