578be62310b4790ebc71a5e8cb014a219a018930
domenico.bressanello <domenico.bressanello@avanade.com>
2022-09-09 20:18:08 +0200

    second implementation specific to this desired function (reversing a sorted list) - implementing same main method but to favour composition separating different functions into classes

--- Design Patterns/Strategy Pattern/Entities/ConcreteStrategyB.cs
+++ Design Patterns/Strategy Pattern/Entities/ConcreteStrategyB.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Design_Patterns.Strategy_Pattern.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace Design_Patterns.Strategy_Pattern.Entities
 {
-    public class ConcreteStrategyB
+    public class ConcreteStrategyB : IStrategy
     {
+        public object Algorithm(object data)
+        {
+            var list = data as List<string>;
+            list.Sort();
+            list.Reverse();
+
+            return list;
+        }
+
     }
 }
