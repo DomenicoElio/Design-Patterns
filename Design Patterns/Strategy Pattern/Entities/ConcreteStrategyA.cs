7f099fc7fbe5efd5bc747b7c67f9a544b2329885
domenico.bressanello <domenico.bressanello@avanade.com>
2022-09-09 20:15:35 +0200

    first strategy (implementation of algorythm favouring composition rather then inheritance - implementing the general method to a specific function)

--- Design Patterns/Strategy Pattern/Entities/ConcreteStrategyA.cs
+++ Design Patterns/Strategy Pattern/Entities/ConcreteStrategyA.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Design_Patterns.Strategy_Pattern.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace Design_Patterns.Strategy_Pattern.Entities
 {
-    public class ConcreteStrategyA
+    public class ConcreteStrategyA : IStrategy
     {
+        public object Algorithm(object data)
+        {
+            var list = data as List<string>;
+            list.Sort();
+
+            return list;
+        }
+
     }
 }
